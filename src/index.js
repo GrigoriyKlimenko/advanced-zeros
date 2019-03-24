e5a966aea3405291d6f6a3207f6d302fddf7d493
GrigoryKlimenko <fageol46@gmail.com>
2019-03-24 08:22:35 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  let a;
+  let b;
+  let c;
+  let zeroArray = [];
+  let zerosCount;
+  for (a = 2; a <= base; a++) {
+    b = 0;
+    c = 0;
+    let number2 = number;
+    if (base % a == 0) {
+      while (base % a == 0) {
+        base = base / a;
+        b++;
+      }
+      while (Math.floor(number2 / a) >= 1) {
+        number2 = Math.floor(number2 / a);
+        c = c + number2;
+      }
+      zeroArray.push(Math.floor(c / b));
+    }
+  }
+  zerosCount = zeroArray[0];
+  for (i = 1; i < zeroArray.length; i++) {
+    if (zeroArray[i] < zerosCount) {
+      zerosCount = zeroArray[i];
+    }
+  }
+  return zerosCount;
 }
